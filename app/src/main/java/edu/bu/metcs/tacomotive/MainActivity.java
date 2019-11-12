a8371c2fa499fed406bd6d595a91b4ef5c31b21e
Larry Barker <lbarker@wwrfresource.com>
2019-11-11 19:49:29 -0600

    refactor firebase auth support

--- app/src/main/java/edu/bu/metcs/tacomotive/MainActivity.java
+++ app/src/main/java/edu/bu/metcs/tacomotive/MainActivity.java
@@ -1,6 +1,5 @@
 package edu.bu.metcs.tacomotive;
 
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
@@ -9,37 +8,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.ActionCodeSettings;
 import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.EmailAuthProvider;
-import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthSettings;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GithubAuthProvider;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.auth.PhoneAuthCredential;
-import com.google.firebase.auth.PhoneAuthProvider;
-import com.google.firebase.auth.PlayGamesAuthProvider;
-import com.google.firebase.auth.SignInMethodQueryResult;
 import com.google.firebase.auth.UserInfo;
 import com.google.firebase.auth.UserProfileChangeRequest;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
     private static final int RC_SIGN_IN = 123;
 
     private static final String TAG = "MainActivity";
@@ -49,45 +36,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        checkCurrentUser();
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
     }
 
     public void onClickSignOut(View view) {
         signOut();
-        checkCurrentUser();
-    }
-
-    public void checkCurrentUser() {
-
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-        if (user != null) {
-
-            usernameTextView = findViewById(R.id.usernameTextViewId);
-            usernameTextView.setText(user.getDisplayName());
-
-        } else {
-
-            List<AuthUI.IdpConfig> providers = Arrays.asList(
-                    new AuthUI.IdpConfig.EmailBuilder().build()
-            );
-
-            // Create and launch sign-in intent
-            startActivityForResult(
-                    AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setAvailableProviders(providers)
-                            .build(),
-                    RC_SIGN_IN
-            );
-
-        }
-
+        updateUI(null);
     }
 
     public void getUserProfile() {
@@ -285,8 +250,27 @@ public class MainActivity extends AppCompatActivity {
         // No-op
     }
 
-
     private void updateUI(@Nullable FirebaseUser user) {
-        // No-op
+        if (user != null) {
+
+            usernameTextView = findViewById(R.id.usernameTextViewId);
+            usernameTextView.setText(user.getDisplayName());
+
+        } else {
+
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.EmailBuilder().build()
+            );
+
+            // Create and launch sign-in intent
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    RC_SIGN_IN
+            );
+
+        }
     }
 }
